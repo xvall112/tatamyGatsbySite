42c4d426d2ae8459aa97516c5abecccd7e646960
Lukas Valta <valtalukas@seznam.cz>
2023-08-05 20:03:04 +0200

    add gtag

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,3 +9,27 @@ export const wrapRootElement = ({ element }) => {
   return <>{element}</>;
 };
  */
+
+// gatsby-ssr.js
+import React from "react";
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      key="ga-script"
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+    />,
+    <script
+      key="ga-init"
+      dangerouslySetInnerHTML={{
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', ${process.env.GOOGLE_ANALYTICS_ID});
+        `,
+      }}
+    />,
+  ]);
+};
