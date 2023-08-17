bd2c2463ae2fab72b2eb63313f419090098e92f1
Lukas Valta <valtalukas@seznam.cz>
2023-08-17 22:51:48 +0200

    delete script

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -124,11 +124,7 @@ const SEO = ({ description, title, image, article, children }) => {
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-ER4QD0EM9D"
       ></script>
-      <script>
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'G-ER4QD0EM9D');
-      </script>
+
       {children}
     </>
   );
