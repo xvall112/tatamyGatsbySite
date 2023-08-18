6d6a5e771c03958d87292cccb08bd5abc82cabed
Lukas Valta <valtalukas@seznam.cz>
2023-08-18 07:19:54 +0200

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,30 +25,30 @@ module.exports = {
     },
   },
   plugins: [
-    // {
-    //   resolve: `gatsby-plugin-google-gtag`,
-    //   options: {
-    //     // You can add multiple tracking ids and a page view event will be fired for all of them.
-    //     trackingIds: [
-    //       "G-ER4QD0EM9D", // Google Analytics / GA
-    //     ],
-    //     // This object gets passed directly to the gtag config command
-    //     // This config will be shared across all trackingIds
-    //     gtagConfig: {
-    //       anonymize_ip: true,
-    //       cookie_expires: 0,
-    //     },
-    //     // This object is used for configuration specific to this plugin
-    //     pluginConfig: {
-    //       // Puts tracking script in the head instead of the body
-    //       head: true,
-    //       // Setting this parameter is also optional
-    //       respectDNT: true,
-    //       // Avoids sending page view hits from custom paths
-    //       exclude: ["/preview/**", "/do-not-track/me/too/"],
-    //     },
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a page view event will be fired for all of them.
+        trackingIds: [
+          "G-ER4QD0EM9D", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending page view hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
     {
       resolve: "gatsby-source-sanity",
       options: {
