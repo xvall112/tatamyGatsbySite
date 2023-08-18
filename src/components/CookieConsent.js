c824f37be1e7cd5373719683b6883d24aed5e039
Lukas Valta <valtalukas@seznam.cz>
2023-08-18 07:23:16 +0200

    remove cookiebot

--- src/components/CookieConsent.js
+++ src/components/CookieConsent.js
@@ -1,20 +1,20 @@
-import React from "react";
+// import React from "react";
 
-const CookieConsent = () => {
-  return (
-    <script
-      id="Cookiebot"
-      src="https://consent.cookiebot.com/uc.js"
-      data-cbid={process.env.COOKIEBOT_ID} // replace with your Cookiebot ID
-      data-blockingmode="auto"
-      type="text/javascript"
-      dangerouslySetInnerHTML={{
-        __html: `
-              // Add any custom Cookiebot configuration code here
-            `,
-      }}
-    />
-  );
-};
+// const CookieConsent = () => {
+//   return (
+//     <script
+//       id="Cookiebot"
+//       src="https://consent.cookiebot.com/uc.js"
+//       data-cbid={process.env.COOKIEBOT_ID} // replace with your Cookiebot ID
+//       data-blockingmode="auto"
+//       type="text/javascript"
+//       dangerouslySetInnerHTML={{
+//         __html: `
+//               // Add any custom Cookiebot configuration code here
+//             `,
+//       }}
+//     />
+//   );
+// };
 
-export default CookieConsent;
+// export default CookieConsent;
